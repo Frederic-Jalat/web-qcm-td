65478ec7e25fc351f3f895da5ecdc8adccdd7f14
Frédéric <fred.jalat-million@laposte.net>
2019-02-07 09:36:13 +0100

    add exercice4

--- exercice/exercice4.php
+++ exercice/exercice4.php
@@ -4,6 +4,25 @@
  * the standard input according to the problem statement.
  **/
 $s = $args[0];
+$result=0;
+$s = str_split($s);
+
+foreach($s as $i) {
+    if ($i === '0') {
+        $result +=1;
+    }
+    if ($i === '6') {
+        $result += 1;
+    }
+    if ($i === '8') {
+        $result += 2;
+    }
+    if ($i === '9') {
+        $result += 1;
+    }
+    
+}
+
 
 // Write an action using echo().
-echo('answer');
+echo $result;
